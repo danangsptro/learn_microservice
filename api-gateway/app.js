97e974d2e076f8785b846b2ec94c55e3747a8d2f
danangsptro <danangsaputro300@gmail.com>
2023-02-16 09:13:52 +0700

    enhance micro

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -7,11 +7,16 @@ const logger = require('morgan')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const coursesRouter = require('./routes/courses')
+const chaptersRouter = require('./routes/chapters')
+const lessonsRouter = require('./routes/lessons')
 const mediaRouter = require('./routes/media')
 const ordersRouter = require('./routes/orders')
 const paymentsRouter = require('./routes/payments')
 const refreshTokensRouter = require('./routes/refreshToken')
 const mentorsRouter = require('./routes/mentors')
+const imageCourses = require('./routes/imageCourse')
+const myCoursesRouter = require('./routes/myCourses')
+const reviewsRouter = require('./routes/reviews')
 
 const verifyToken = require('./middlewares/verifyToken')
 
@@ -24,12 +29,17 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter) 
-app.use('/courses', verifyToken, coursesRouter)
+app.use('/users', usersRouter)
+app.use('/courses', coursesRouter)
+app.use('/chapters', verifyToken, chaptersRouter)
+app.use('/lessons', verifyToken, lessonsRouter)
 app.use('/media', mediaRouter)
 app.use('/orders', ordersRouter)
 app.use('/payments', paymentsRouter)
-app.use('/refresh-tokens', refreshTokensRouter) 
-app.use('/mentors',verifyToken, mentorsRouter);
+app.use('/refresh-tokens', refreshTokensRouter)
+app.use('/mentors', verifyToken, mentorsRouter);
+app.use('/image-courses', verifyToken, imageCourses);
+app.use('/my-courses', verifyToken, myCoursesRouter)
+app.use('/reviews', verifyToken, reviewsRouter)
 
 module.exports = app
