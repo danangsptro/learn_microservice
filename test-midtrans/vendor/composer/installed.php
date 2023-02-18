ccd87418b5bcd4e23b754ed9f4cfbfd6c580856c
danangsptro <danangsaputro300@gmail.com>
2023-02-18 10:24:22 +0700

    done create api which need

---
+++ test-midtrans/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '97e974d2e076f8785b846b2ec94c55e3747a8d2f',
+        'name' => 'user/test-midtrans',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'midtrans/midtrans-php' => array(
+            'pretty_version' => '2.5.2',
+            'version' => '2.5.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../midtrans/midtrans-php',
+            'aliases' => array(),
+            'reference' => 'a1ad0c824449ca8c68c4cf11b3417ad518311d2b',
+            'dev_requirement' => false,
+        ),
+        'user/test-midtrans' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '97e974d2e076f8785b846b2ec94c55e3747a8d2f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
